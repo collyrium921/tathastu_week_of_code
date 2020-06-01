21aa7ef373071a9401f97c55fc40fe43271fccf6
kaju-1234 <47491966+kaju-1234@users.noreply.github.com>
2020-06-01 13:15:43 +0530

    Create program5.py

---
+++ day6/program5.py
@@ -0,0 +1,30 @@
+MAX = 100005
+fibonacci = set()  
+def createHash(): 
+    global fibonacci 
+
+    prev , curr = 0, 1
+    fibonacci.add(prev) 
+    fibonacci.add(curr) 
+    while (curr <= MAX): 
+        temp = curr + prev 
+        if temp <= MAX: 
+            fibonacci.add(temp) 
+        prev = curr 
+        curr = temp 
+def checkArray(arr, n):  
+    sum = 0 
+    for i in range( n ): 
+        if (arr[i] in fibonacci): 
+            sum += arr[i]  
+    if (sum in fibonacci): 
+        return True
+    return False 
+arr = list(map(int, input().split())) 
+n = len(arr) 
+createHash() 
+
+if (checkArray(arr, n)): 
+    print("Yes") 
+else: 
+    print("No")  
